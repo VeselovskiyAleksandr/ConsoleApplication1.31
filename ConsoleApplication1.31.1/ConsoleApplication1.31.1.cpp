b63f4d435418048f3a8289715fbec614d22ff403
VeselovskiyAleksandr <79366865+VeselovskiyAleksandr@users.noreply.github.com>
2022-08-08 16:11:19 +0300

    test 1.

--- ConsoleApplication1.31.1/ConsoleApplication1.31.1.cpp
+++ ConsoleApplication1.31.1/ConsoleApplication1.31.1.cpp
@@ -12,17 +12,28 @@ using namespace std;
 vector <string> swimHistory;
 mutex broadcast_of_swim_access;
 
-void broadcast_of_swim(int counttime, string name, float speed, float distance, int result) {
-	while (distance < 100) {	
+struct swimmers {
+	string name;
+	int result;
+	float speed;
+	float distance;
+};
+swimmers swimmer[6];
+
+void broadcast_of_swim( string name, float speed ) {
+	float distance = 0;
+	int result, counttime=1;
+	while (true) {
 		this_thread::sleep_for(chrono::seconds(1));
-		distance = counttime * speed;
-		if ((distance >= 100) && ((counttime - 1) * speed) < 100) {
+		distance = (float)counttime * speed;
+		if ((distance >= 100) && ((float)(counttime - 1) * speed) < 100) {
 			result = counttime;
 	broadcast_of_swim_access.lock();
 			swimHistory.push_back(name);
 			swimHistory.push_back(to_string(result));
 			cout << "\nПловец " << name << " финишировал.";
 		broadcast_of_swim_access.unlock();
+		break;
 		}
 		else {
 			broadcast_of_swim_access.lock();
@@ -36,30 +47,24 @@ void broadcast_of_swim(int counttime, string name, float speed, float distance, 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	string name[6];
-	int result[6];
-	float speed[6];
-	float distance[6];
 	int counttime = 1;
-		thread swim[6];
 	cout << "Укажите имена пловцов, участвующих в заплыве.\n";
 	for (int i = 0; i < 6; ++i) {
-		cin >> name[i];
+		cin >> swimmer[i].name;
 	}
 	cout << "Укажите скорости пловцов.\n";
 	for (int i = 0; i < 6; ++i) {
-		cin >> speed[i];
+		cin >> swimmer[i].speed;
 	}
-
 	cout << "\n           Все nловцы на старте.";
 	cout << "\n                 СТАРТ!!!\n";
 
-		thread swim1(broadcast_of_swim, counttime, name[0], speed[0], distance[0], result[0]);
-		thread swim2(broadcast_of_swim, counttime, name[1], speed[1], distance[1], result[1]);
-		thread swim3(broadcast_of_swim, counttime, name[2], speed[2], distance[2], result[2]);
-		thread swim4(broadcast_of_swim, counttime, name[3], speed[3], distance[3], result[3]);
-		thread swim5(broadcast_of_swim, counttime, name[4], speed[4], distance[4], result[4]);
-		thread swim6(broadcast_of_swim, counttime, name[5], speed[5], distance[5], result[5]);
+	thread swim1(broadcast_of_swim,  swimmer[0].name, swimmer[0].speed);
+	thread swim2(broadcast_of_swim, swimmer[1].name, swimmer[1].speed);
+	thread swim3(broadcast_of_swim, swimmer[2].name, swimmer[2].speed);
+	thread swim4(broadcast_of_swim,  swimmer[3].name, swimmer[3].speed);
+	thread swim5(broadcast_of_swim, swimmer[4].name, swimmer[4].speed);
+	thread swim6(broadcast_of_swim,  swimmer[5].name, swimmer[5].speed);
 		swim1.join();
 		swim2.join();
 		swim3.join();
@@ -69,7 +74,7 @@ int main()
 		cout << "\n";
 	cout << "\nРезультаты участников заплыва: ";
 	for (int i = 0; i < swimHistory.size(); i +=2) {
-		cout << "\n " << i / 2 + 1 << " место " << swimHistory[i] << " результат " << swimHistory[i + 1];
+		cout << "\n " << i / 2 + 1 << " место " << swimHistory[i] << " результат " << swimHistory[i + 1]<<" секунд.";
 	}
 }
 
